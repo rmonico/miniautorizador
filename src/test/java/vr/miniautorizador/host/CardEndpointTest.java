d23dda473f6114c941f614bea12ee39e3fb0209a
Rafael Monico <rmonico@moningrado>
2022-10-25 22:30:01 -0300

    test: Testes do endpoint de saldo do cartão

---
+++ src/test/java/vr/miniautorizador/host/CardEndpointTest.java
@@ -0,0 +1,35 @@
+package vr.miniautorizador.host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import vr.miniautorizador.service.CardService;
+
+import static java.util.Optional.of;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static vr.miniautorizador.model.MockedCard.*;
+
+@WebMvcTest(CardEndpoint.class)
+public class CardEndpointTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CardService service;
+
+    @Test
+    void getCardBalance() throws Exception {
+        when(service.getBalance(CARD_NUMBER)).thenReturn(of(defaultCard().getSaldo()));
+
+        mvc.perform(
+                get("/cartoes/{numeroCartao}", CARD_NUMBER))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.saldo").value(CARD_BALANCE));
+    }
+}
