57004ba717fd3872be35c1de2be0f7a9318fd29c
Rafael Monico <rmonico@moningrado>
2022-10-25 22:27:05 -0300

    test: Extração da classe de mock de cartão

---
+++ src/test/java/vr/miniautorizador/model/MockedCard.java
@@ -0,0 +1,20 @@
+package vr.miniautorizador.model;
+
+import org.bson.types.ObjectId;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class MockedCard {
+    public static final String CARD_NUMBER = "1234";
+    public static final BigDecimal CARD_BALANCE = new BigDecimal("19.99");
+    public static final String CARD_PASS = "1q2w3e4r";
+
+    public static Card defaultCard() {
+        return new Card(new ObjectId(), CARD_NUMBER, CARD_PASS, CARD_BALANCE);
+    }
+
+    public static Optional<Card> defaultCardOpt() {
+        return Optional.of(defaultCard());
+    }
+}
--- src/test/java/vr/miniautorizador/service/impl/CardServiceImplTest.java
+++ src/test/java/vr/miniautorizador/service/impl/CardServiceImplTest.java
@@ -1,12 +1,10 @@
 package vr.miniautorizador.service.impl;
 
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import vr.miniautorizador.model.Card;
 import vr.miniautorizador.repository.CardRepository;
 
 import java.math.BigDecimal;
@@ -15,13 +13,11 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+import static vr.miniautorizador.model.MockedCard.CARD_NUMBER;
+import static vr.miniautorizador.model.MockedCard.defaultCardOpt;
 
 @ExtendWith(MockitoExtension.class)
 class CardServiceImplTest {
-
-    private static final String CARD_NUMBER = "1234";
-    private static final BigDecimal CARD_BALANCE = new BigDecimal("19.99");
-    private static final String CARD_PASS = "1q2w3e4r";
 
     @InjectMocks
     private CardServiceImpl service;
@@ -29,14 +25,9 @@ class CardServiceImplTest {
     @Mock
     private CardRepository repository;
 
-    private Card makeCard() {
-        return new Card(new ObjectId(), CARD_NUMBER, CARD_PASS, CARD_BALANCE);
-    }
-
     @Test
     void GIVEN_card_number_WHEN_it_exists_THEN_return_its_balance() {
-        Optional<Card> theCard = Optional.of(makeCard());
-        when(repository.findByNumero(CARD_NUMBER)).thenReturn(theCard);
+        when(repository.findByNumero(CARD_NUMBER)).thenReturn(defaultCardOpt());
 
         Optional<BigDecimal> balance = service.getBalance(CARD_NUMBER);
 
