87220d082107353c1d22164709b713180bde3352
Rafael Monico <rmonico@moningrado>
2022-10-27 01:15:45 -0300

    test: Testes da service de transação com saldo insuficiente

--- src/main/java/vr/miniautorizador/service/impl/CardServiceImpl.java
+++ src/main/java/vr/miniautorizador/service/impl/CardServiceImpl.java
@@ -3,6 +3,7 @@ package vr.miniautorizador.service.impl;
 import lombok.val;
 import org.springframework.stereotype.Service;
 import vr.miniautorizador.exception.ExistingCardException;
+import vr.miniautorizador.exception.InsufficientBalance;
 import vr.miniautorizador.exception.InvalidCardNumber;
 import vr.miniautorizador.exception.InvalidPassword;
 import vr.miniautorizador.model.Card;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
+import static java.math.BigDecimal.valueOf;
 
 @Service
 public class CardServiceImpl implements CardService {
@@ -68,5 +70,6 @@ public class CardServiceImpl implements CardService {
     private void authorize(Transaction t, Optional<Card> card) {
         card.orElseThrow(InvalidCardNumber::new);
         card.map(Card::getSenha).filter(s -> s.equals(t.getSenhaCartao())).orElseThrow(InvalidPassword::new);
+        card.map(Card::getSaldo).filter(s -> s.compareTo(t.getValor()) >= 0).orElseThrow(InsufficientBalance::new);
     }
 }
